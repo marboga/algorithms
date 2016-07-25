3b1faf4af550152ba24db871f43957659419f476
Michael Arbogast <marboga@gmail.com>
2016-07-25 10:02:41 -0700

    building out string.concat and string.slice

---
+++ stringsDay4.js
@@ -0,0 +1,14 @@
+// Strings and Associative Arrays Day 4
+// Recreate these built-in functions from JavaScript’s string library:
+// String.concat
+// string1.concat(str2,str3,...,strX) - add string(s) to end of existing one. Return new string.
+// String.slice
+// string.slice(start,end) - extract part of a string and return in a new one. Start and end are indices into the string, with the first character at index 0. End param is optional and, if present, refers to one beyond the last character to include.
+// Bonus: include support for negative indices, representing offsets from string-end. Example: string.slice(-1) returns the string’s last character.
+
+function concat(str1, ...strX) {
+	var newString = ""
+	for (var i = 0; i < Function.length; i++) {
+		
+	}
+}
