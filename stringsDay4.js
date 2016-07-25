82e48127cb8d9150e17840b677c0da8e7ffe03b2
Michael Arbogast <marboga@gmail.com>
2016-07-25 10:45:41 -0700

    finished concat and slice functions

--- stringsDay4.js
+++ stringsDay4.js
@@ -6,9 +6,26 @@
 // string.slice(start,end) - extract part of a string and return in a new one. Start and end are indices into the string, with the first character at index 0. End param is optional and, if present, refers to one beyond the last character to include.
 // Bonus: include support for negative indices, representing offsets from string-end. Example: string.slice(-1) returns the string’s last character.
 
-function concat(str1, ...strX) {
+function concat(str1, str2, strX) {
 	var newString = ""
-	for (var i = 0; i < Function.length; i++) {
-		
+	console.log(arguments)
+	for (var i = 0; i < arguments.length; i++) {
+		newString += arguments[i]
 	}
+	return newString
 }
+console.log(concat("x", "y", "z", "abc"))
+
+String.prototype.slyce = function(string, beginDex, enDex) {
+	var newString = ""
+	if (!enDex || string.length < enDex) {
+		enDex = string.length;
+	}
+	for (var i = beginDex; i < enDex; i++) {
+		newString += string[i];
+	}
+	return newString;
+}
+
+var stringo = "candy and cookies and pie"
+console.log(stringo.slyce(stringo, 1,10))
